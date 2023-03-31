29cfad9405ff606385e71677cd4373db56b074c0
HHHUUUGGGOOO <b07901103@ntu.edu.tw>
2023-03-31 22:18:33 +0800

    proveCmd.cpp done

--- src/prove/proveCmd.cpp
+++ src/prove/proveCmd.cpp
@@ -215,7 +215,6 @@ PCheckPropertyCmd::exec(const string& option) {
     }
 
     BddNodeV monitor = bddMgrV->getBddNodeV(netId.id);
-    // if (netId.fanin0Cp) monitor = ~monitor;
     assert(monitor());
     bddMgrV->runPCheckProperty(gvNtkMgr->getNetNameFromId(netId.id), monitor);
 
