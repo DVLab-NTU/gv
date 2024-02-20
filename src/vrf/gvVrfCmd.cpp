0385781bb011218c2090b63ca40a15e120f149b8
hchchiu <robert890619@gmail.com>
2024-02-20 13:39:07 +0800

    Add the pdr command

--- src/vrf/gvVrfCmd.cpp
+++ src/vrf/gvVrfCmd.cpp
@@ -10,6 +10,8 @@
 #include <string>
 
 #include "abcMgr.h"
+#include "cirDef.h"
+#include "cirMgr.h"
 #include "gvModMgr.h"
 #include "gvMsg.h"
 #include "gvUsage.h"
@@ -18,7 +20,8 @@
 using namespace std;
 
 bool initVrfCmd() {
-    return (gvCmdMgr->regCmd("Formal Verify", 1, 1, new GVFormalVerifyCmd));
+    return (gvCmdMgr->regCmd("Formal Verify", 1, 1, new GVFormalVerifyCmd)) &&
+           (gvCmdMgr->regCmd("PDR", 3, new GVPdrCmd));
 }
 
 //------------------------------------------------------------------------------------------------------------
@@ -636,6 +639,40 @@ void GVFormalVerifyCmd ::usage(const bool& verbose) const {
 void GVFormalVerifyCmd ::help() const {
     cout << setw(20) << left << "Formal Verify: "
          << "Use options to execute specific formal engine." << endl;
+}
+
+//----------------------------------------------------------------------
+//    PDR [-Verbose]
+//----------------------------------------------------------------------
+GVCmdExecStatus GVPdrCmd::exec(const string& option) {
+    // check option
+    vector<string> options;
+    GVCmdExec::lexOptions(option, options);
+
+    bool verbose = false;
+    string fileName;
+    for (size_t i = 0, n = options.size(); i < n; ++i) {
+        if (myStrNCmp("-Verbose", options[i], 2) == 0)
+            verbose = true;
+    }
+
+    if (cirMgr) {
+        IDMap aigIdMap;
+        cirMgr->reorderGateId(aigIdMap);
+        abcMgr->cirToAig(aigIdMap);
+        abcMgr->runPDR(verbose);
+    }
+
+    return GV_CMD_EXEC_DONE;
+}
+
+void GVPdrCmd::usage(const bool& verbose) const {
+    cout << "Usage: PDR [-Verbose]" << endl;
+}
+
+void GVPdrCmd::help() const {
+    cout << setw(20) << left << "PDR:"
+         << "Model checking using property directed reachability in abc." << endl;
 }
 
 #endif
