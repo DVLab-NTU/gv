8935c4df2e6feb14184bb6c41e9c28bcac6b2957
hchchiu <robert890619@gmail.com>
2024-03-21 00:58:35 +0800

    Update platform detection method

--- patches/yosys_patch.cmake
+++ patches/yosys_patch.cmake
@@ -1,14 +1,15 @@
 # Path to the yosys patch
-if(${MACOSX})
+if(APPLE)
+    message(STATUS "Using Mac Patch")
     set(YOSYS_PATCH_FILE yosys_mac.patch)
 else()
+    message(STATUS "Using Linux Patch")
     set(YOSYS_PATCH_FILE yosys_linux.patch)
 endif()
-set(OUTPUT_REDIRECT "/dev/null")
-# message(FATAL_ERROR ${YOSYS_PATCH_FILE})
 
-# Command for checking the patch is applied or not set(YOSYS_PATCH_CHECK git apply --check -q
-# ${CMAKE_CURRENT_LIST_DIR}/${YOSYS_PATCH_FILE})
+set(OUTPUT_REDIRECT "/dev/null")
+
+# Command for checking the patch is applied or not
 set(YOSYS_PATCH_CHECK git apply --check ${CMAKE_CURRENT_LIST_DIR}/${YOSYS_PATCH_FILE})
 # Command for applying the patch
 set(YOSYS_PATCH git apply ${CMAKE_CURRENT_LIST_DIR}/${YOSYS_PATCH_FILE})
@@ -25,9 +26,9 @@ execute_process(
 # Patch has not been applied
 if(STATUS EQUAL "0")
     # Apply the patch to the Yosys Makefile
-    message(WARNING "Applying the yosys patch !!")
+    message(STATUS "Applying the yosys patch")
     execute_process(COMMAND ${YOSYS_PATCH})
     # Patch has been applied
 else()
-    message(WARNING "Skip the patch step !!\nPatch has been applied !!")
+    message(STATUS "Skip the patch step\nPatch has been applied")
 endif()
