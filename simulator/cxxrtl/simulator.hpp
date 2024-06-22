704557d046778435178b965e9e544176e8e80db4
hchchiu <robert890619@gmail.com>
2024-06-22 21:26:08 +0800

    Add the CXXRTL template code

--- simulator/cxxrtl/simulator.hpp
+++ simulator/cxxrtl/simulator.hpp
@@ -7,19 +7,20 @@
 #ifndef simulator_h
 #define simulator_h
 
-#include <backends/cxxrtl/cxxrtl_vcd.h>
-
 #include <cstddef>
 #include <fstream>
 #include <iostream>
 
-#include ".sim.cpp"
 #include "backends/cxxrtl/cxxrtl.h"
+#include "backends/cxxrtl/cxxrtl_vcd.h"
 #include "interface.hpp"
 #include "utility.hpp"
 
 #define GREEN_TEXT "\033[32m"  // ANSI escape code for green color
 #define RESET_COLOR "\033[0m"  // ANSI escape code to reset color
+
+#define STR(s) STR2(s)
+#define STR2(s) #s
 
 class Simulator {
     typedef std::vector<std::vector<unsigned>> Pattern;  // Input pattern
@@ -28,16 +29,23 @@ public:
     Simulator(int c) : cycle(c), sampleCount(0), isVCD(true) { init(); }
 
     bool init() {
-        top.debug_info(all_debug_items);
+        itf = new Interface(&all_debug_items);
+
         vcd.timescale(1, "ps");
         vcd.add_without_memories(all_debug_items);
         vcd.add(all_debug_items);
-        waves.open("cxxrtl.vcd");
+
+        patternFile = STR(PATTERN_FILE);
+        vcdFile = STR(VCD_FILE);
+        return true;
+    }
+
+    bool enableVCD() {
+        waves.open(vcdFile);
         if (!waves.is_open()) {
             return false;
             std::cout << " Open the CXXRTL file failed !!\n";
         }
-        itf = new Interface(&all_debug_items);
         return true;
     }
 
@@ -48,7 +56,8 @@ public:
 
     void resetNeg() {
         vcd.sample(sampleCount++);
-        top.step();
+        itf->step();
+
         //          __    __
         // clk:  __|  |__|  |__
         //                   __
@@ -77,12 +86,11 @@ public:
     }
 
     //! setter functions
-    void setCLK(const bool &val) { itf->clk[0].setValue(val); }
-    void setRST(const bool &val) { itf->rst[0].setValue(val); }
+    void setCLK(const bool &value) { itf->clk[0].setValue(value); }
+    void setRST(const bool &value) { itf->rst[0].setValue(value); }
     void setPiPattern(std::vector<unsigned> &piPattern) {
         for (unsigned i = 0; i < piPattern.size(); i++) {
             itf->pi[i].setValue(piPattern[i]);
-            std::cout << " PI NAME: " << itf->pi[i].name << " Value: " << piPattern[i] << "\n";
         };
     }
 
@@ -90,12 +98,9 @@ public:
     unsigned getPiNum() { return itf->pi.size(); }
     unsigned getPoNum() { return itf->po.size(); }
     unsigned getPiUpper(const unsigned &idx) { return itf->pi[idx].getUpper(); }
-    void setRandVal() {
-        top.p_b.set<unsigned>(rGen.getRandNum(0, 16));
-        top.p_a.set<unsigned>(rGen.getRandNum(0, 16));
-    }
+
     void eval() {
-        top.step();
+        itf->step();
         if (isVCD) {
             vcd.sample(sampleCount++);
             outVCD();
@@ -124,6 +129,8 @@ public:
         std::cout << "sum= " << sum << "\n";
         std::cout << "clk = " << itf->clk[0].getValue() << "\n";
         std::cout << "rst= " << itf->rst[0].getValue() << "\n";
+        std::cout << itf->pi[0].getName() << "= " << itf->pi[0].getValue() << "\n";
+        std::cout << itf->pi[1].getName() << "= " << itf->pi[1].getValue() << "\n";
         std::cout << std::endl;
     }
 
@@ -137,36 +144,59 @@ public:
         return pattern;
     }
 
+    std::string replaceXValue(const std::string &pattern) {
+        std::string newPattern = pattern;
+        size_t xPos = newPattern.find('X');
+        while (xPos != std::string::npos) {
+            newPattern[xPos] = '0';
+            xPos = newPattern.find('X');
+        }
+        return newPattern;
+    }
+
     bool loadRandomPattern() {
         for (int i = 0; i < cycle; ++i) {
             std::vector<unsigned> onePattern = genPiRandomPattern();
-            patternVec.push_back(onePattern);
+            patternVec.emplace_back(onePattern);
         }
         return true;
     }
 
-    // void dump_all_items() {
-    //     std::cout << "All items: \n";
-    //     for (auto &it : all_debug_items.table) {
-    //         for (auto &part : it.second) {
-    //             if (it.first == "a" || it.first == "b") {
-    //                 set4BitValue(part);
-    //             }
-    //             std::cout << it.first
-    //                       << ": type = " << part.type
-    //                       << ", width = " << part.width
-    //                       << ", depth = " << part.depth
-    //                       << ", lsb_at = " << part.lsb_at
-    //                       << ", value = " << *(part.next)
-    //                       << ", zero_at = " << part.zero_at << std::endl;
-    //         }
-    //     }
-    //     std::cout << std::endl;
-    // }
+    bool loadInputPattern() {
+        std::ifstream infile(patternFile);
+        std::string buffer = "", inputStr = "";
+        unsigned width = 0, inputVal = 0;
+
+        if (!infile) {
+            std::cout << "ERROR: Cannot open file " << patternFile << " !!\n";
+            return false;
+        }
+
+        while (infile >> buffer) {
+            std::vector<unsigned> onePattern;
+            for (int i = 0, n = getPiNum(); i < n; ++i) {
+                width = itf->pi[i].getWidth();
+                inputStr = replaceXValue(buffer.substr(0, width));
+                if (inputStr.size() < width) {
+                    std::cout << "ERROR: Illegal input pattern file !!\n";
+                    return false;
+                }
+                inputVal = stoi(inputStr, nullptr, 2);
+                onePattern.emplace_back(inputVal);
+                buffer = buffer.substr(width);
+            }
+            if (buffer.size() != 0) {
+                std::cout << "ERROR: Illegal input pattern file !!\n";
+                return false;
+            }
+            patternVec.push_back(onePattern);
+        }
+        infile.close();
+        return true;
+    }
 
     void startSim(const bool &verbose) {
         int simCycle = patternVec.size();
-        std::cout << "LOAD RANDOM PATTERN: " << simCycle << "\n";
         if (itf->rst.size() > 0)
             resetNeg();
         for (int i = 0; i < simCycle; ++i) {
@@ -176,15 +206,16 @@ public:
         }
     }
 
+    bool isVCD;
     size_t cycle;
     size_t sampleCount;
-    cxxrtl_design::p_adder top;
     cxxrtl::debug_items all_debug_items;
     cxxrtl::vcd_writer vcd;
+    std::string patternFile;
+    std::string vcdFile;
     std::ofstream waves;
     Pattern patternVec;
     Interface *itf;
-    bool isVCD;
 };
 
 #endif /* simulator_h */
