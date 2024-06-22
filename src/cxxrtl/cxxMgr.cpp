553a171f08ec5a565cf2d101818f2e9c81bdec54
hchchiu <robert890619@gmail.com>
2024-06-22 21:27:43 +0800

    Add the CXXRTL manager

--- src/cxxrtl/cxxMgr.cpp
+++ src/cxxrtl/cxxMgr.cpp
@@ -1,5 +1,6 @@
 #include "cxxMgr.h"
 
+#include <cstddef>
 #include <experimental/filesystem>
 #include <fstream>
 #include <iostream>
@@ -9,53 +10,93 @@
 
 #include "fmt/color.h"
 #include "fmt/core.h"
+#include "simMgr.h"
 #include "yosysMgr.h"
+
+CXXMgr* cxxMgr = nullptr;
 
 static string genItfFuncCode(const unsigned& bitWidth) {
     return fmt::format("{0:<4}SET_VALUE_FUNC({1},{2});\n", "", bitWidth, "unsigned");
 }
-
 static string genItfCondCode(const unsigned& bitWidth) {
     return fmt::format("{0:<8}SET_WIDTH_CONDITION({1});\n", "", bitWidth);
 }
-
 static string genItfSignalCode(const string& signaType, const string& signalName) {
     return fmt::format("{0:<8}SET_CXX_SIGNAL({1}, \"{2}\");\n", "", signaType, signalName);
 }
+static string genItfTopObjCode(const string& topModule) {
+    return fmt::format("{0:<4}SET_CXX_TOP({1});\n", "", topModule);
+}
+static string genMacro(const string& macro, const string& value) {
+    return fmt::format("{0}={1} ", macro, value);
+}
+static string genMacro(const string& macro, const size_t& value) {
+    return fmt::format("{0}={1} ", macro, value);
+}
 
-CXXMgr::CXXMgr() {
-    _itfTmpFile = PATH(std::string(GV_CXXRTL_PATH)) / "template/interface.hpp";
-    _itfFile = _itfTmpFile.parent_path().parent_path() / "interface.hpp";
+CXXMgr::CXXMgr() : SimMgr(0) {
+    _cxxDir = PATH(std::string(GV_CXXRTL_PATH)).parent_path();
+    _itfTmpFile = _cxxDir / "template/interface.hpp";
+    _itfFile = _cxxDir / "interface.hpp";
+    _cxxSimFile = _cxxDir / ".sim.cpp";
 }
 
 bool CXXMgr::preCXXSim(const bool& verbose) {
+    if (!genMakeMacro(true)) {
+        fmt::print(fmt::fg(fmt::color::red),
+                   "ERROR: Cannot generate the macro !!\n");
+        return false;
+    }
+    // generate the driver file by calling the Yosys CXXRTL command
+    if (!genCXXDriver(true)) {
+        fmt::print(fmt::fg(fmt::color::red),
+                   "ERROR: Cannot generate the CXXRTL driver file !!\n");
+        return false;
+    }
     // generate the interface code
-    genCXXItf(true);
+    if (!genCXXItf(true)) {
+        fmt::print(fmt::fg(fmt::color::red),
+                   "ERROR: Cannot generate the interface file !!\n");
+        return false;
+    }
+    // generate the simulation execution file
+    if (!genCXXExe(true)) {
+        fmt::print(fmt::fg(fmt::color::red),
+                   "ERROR: Cannot generate the CXXRTL driver file !!\n");
+        return false;
+    }
+    return true;
+}
+
+bool CXXMgr::genMakeMacro(const bool& verbose) {
+    namespace fs = std::experimental::filesystem;
+    PATH currPath = fs::current_path();
+    _macro += genMacro("GV_PATH", currPath);
+    _macro += genMacro("CYCLE", getSimCycle());
+    _macro += genMacro("VCD_FILE", currPath / getVcdFileName());
+    return true;
 }
 
 bool CXXMgr::genCXXItf(const bool& verbose) {
     std::ifstream infile(_itfTmpFile, std::ios::in);
-    std::vector<std::string> condCode;
-    unordered_set<int> uniqBit;
     std::ofstream outfile(_itfFile, std::ios::out);
+    std::vector<std::string> condCode;
     std::string tmpLine;
+    unordered_set<int> uniqBit;
+
     if (!infile) {
-        fmt::print(fg(fmt::color::red) | fmt::emphasis::bold, "Cannot open design {0} !!",
-                   _itfFile.string());
+        fmt::print(fg(fmt::color::red) | fmt::emphasis::bold,
+                   "Cannot open design {0} !!", _itfFile.string());
         return false;
     }
     while (infile) {
         getline(infile, tmpLine);
-        // outfile << tmpLine;
-        // std::cout << tmpLine << std::endl;
         outfile << tmpLine << std::endl;
         if (tmpLine.find("Set signal value functions") != std::string::npos) {
-            // #define SET_CXX_VALUE(bitwidth, type)
             for (int i = 0; i < yosysMgr->getNumPIs(); ++i) {
                 unsigned bitWidth = yosysMgr->getPi(i)->getWidth();
-                if (!uniqBit.count(bitWidth)) {
+                if (bitWidth != 1 && !uniqBit.count(bitWidth)) {
                     uniqBit.insert(bitWidth);
-                    // std::cout << genItfFuncCode(bitWidth);
                     outfile << genItfFuncCode(bitWidth);
                     condCode.emplace_back(genItfCondCode(bitWidth));
                 }
@@ -63,39 +104,77 @@ bool CXXMgr::genCXXItf(const bool& verbose) {
         } else if (tmpLine.find("Set N bits value condition") != std::string::npos) {
             for (int i = 0, n = condCode.size(); i < n; ++i)
                 outfile << condCode[i];
-            // std::cout << condCode[i];
+        } else if (tmpLine.find("Set top design object") != std::string::npos) {
+            outfile << genItfTopObjCode(yosysMgr->getTopModuleName());
         } else if (tmpLine.find("CLK") != std::string::npos) {
-            for (int i = 0; i < yosysMgr->getNumCLKs(); ++i) {
+            for (int i = 0; i < yosysMgr->getNumCLKs(); ++i)
                 outfile << genItfSignalCode("clk", yosysMgr->getClk(i)->getName());
-                // std::cout << genItfSignalCode("clk", yosysMgr->getClk(i)->getName());
-            }
         } else if (tmpLine.find("RST") != std::string::npos) {
             for (int i = 0; i < yosysMgr->getNumRSTs(); ++i)
                 outfile << genItfSignalCode("rst", yosysMgr->getRst(i)->getName());
-            // std::cout << genItfSignalCode("rst", yosysMgr->getRst(i)->getName());
         } else if (tmpLine.find("PI") != std::string::npos) {
             for (int i = 0; i < yosysMgr->getNumPIs(); ++i)
                 outfile << genItfSignalCode("pi", yosysMgr->getPi(i)->getName());
-            // std::cout << genItfSignalCode("pi", yosysMgr->getPi(i)->getName());
         } else if (tmpLine.find("PO") != std::string::npos) {
             for (int i = 0; i < yosysMgr->getNumPOs(); ++i)
                 outfile << genItfSignalCode("po", yosysMgr->getPo(i)->getName());
-            // std::cout << genItfSignalCode("po", yosysMgr->getPo(i)->getName());
         }
         // } else if (tmpLine.find("REG") != std::string::npos) {
         //     for (int i = 0; i < yosysMgr->getNumREGs(); ++i) {
         //         std::string regName = yosysMgr->getTopModuleName() + "__DOT__";
-        //         std::cout << genItfSignalCode("rst", yosysMgr->getRst(i)->getName());
-        //         outfile << genItfCode("reg", yosysMgr->getReg(i)) << endl;
+        //         // std::cout << genItfSignalCode("rst", yosysMgr->getRst(i)->getName());
+        //         // outfile << genItfCode("reg", yosysMgr->getReg(i)) << endl;
         //     }
         // }
     }
     infile.close();
-    // outfile.close();
+    outfile.close();
     return true;
 }
 
-void CXXMgr::fileSim(const bool&) {}
-void CXXMgr::randomSim(const bool&) {
-    preCXXSim(true);
+bool CXXMgr::genCXXDriver(const bool& verbose) {
+    // Call the Yosys command to write the driver.cpp for simulation
+    string writeCXXCmd = "write_cxxrtl " + _cxxSimFile.string();
+    yosysMgr->runPass(writeCXXCmd);
+    return true;
+}
+
+bool CXXMgr::genCXXExe(const bool& verbose) {
+    string compileCmd = fmt::format("make -C {0} {1}", _cxxDir.string(), _macro);
+    if (system(compileCmd.c_str()) != 0) {
+        fmt::print(fmt::fg(fmt::color::red),
+                   "ERROR: Cannot execute the system command \"{0}\" !!\n", compileCmd);
+        return false;
+    }
+    return true;
+}
+
+bool CXXMgr::runCXXSim(const bool& verbose) {
+    string runCmd = fmt::format("make -C {0} run", _cxxDir.string());
+    if (system(runCmd.c_str()) != 0) {
+        fmt::print(fmt::fg(fmt::color::red),
+                   "ERROR: Cannot execute the system command \"{0}\" !!\n", runCmd);
+        return false;
+    }
+    return true;
+}
+
+/**
+ * @brief Add the MODE=1 and PATTERN_FILE macro
+ *
+ */
+void CXXMgr::enableFileSim() {
+    _macro += genMacro("MODE", "1");
+    _macro += genMacro("PATTERN_FILE", getPatternFileName());
+}
+
+void CXXMgr::fileSim(const bool& verbose) {
+    enableFileSim();
+    if (preCXXSim(verbose))
+        runCXXSim(verbose);
+}
+
+void CXXMgr::randomSim(const bool& verbose) {
+    if (preCXXSim(verbose))
+        runCXXSim(verbose);
 }
--- src/cxxrtl/cxxMgr.h
+++ src/cxxrtl/cxxMgr.h
@@ -16,26 +16,25 @@ class CXXMgr : public SimMgr {
 public:
     CXXMgr();
     bool preCXXSim(const bool&);
-    bool runVrltSim(const bool&);
+    bool runCXXSim(const bool&);
     void fileSim(const bool&);
     void randomSim(const bool&);
 
 private:
     PATH _itfTmpFile;
     PATH _itfFile;
-    // std::string _itfPath;      // interface.hpp path
-    std::string _itfFileName;  // save the name of interface.hpp file
-    std::string _dirPath;      // verilator directory path
-    std::string _designPath;   // input design path
-    std::string _macro;        // save the GV_PATH and Design Info to Verilator with macro
-    std::string _make;         // save the "make" command
-    std::string _verboseCmd;   // save the command for not printing command message
+    PATH _cxxSimFile;
+    PATH _cxxDir;
+    std::string _macro;       // save the GV_PATH and Design Info to Verilator with macro
+    std::string _make;        // save the "make" command
+    std::string _verboseCmd;  // save the command for not printing command message
 
     // private member functions for simluation on Verilator
     bool preDesignInfo(const bool&);
-    bool genVrltBuild(const bool&);
+    bool genMakeMacro(const bool&);
     bool genCXXItf(const bool&);
-    bool genVrltMakefile(const bool&);
+    bool genCXXDriver(const bool&);
+    bool genCXXExe(const bool&);
     // private member functions to enable Simulation mode
     void enableFileSim();
     void enableRandomSim();
