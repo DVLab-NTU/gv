86967dd43ed993440a973a9663924b63880f315a
hchchiu <robert890619@gmail.com>
2024-09-01 22:32:20 +0800

    remove the debugging code

--- src/exp/expCmd.cpp
+++ src/exp/expCmd.cpp
@@ -3,14 +3,10 @@
 
 #include "expCmd.h"
 
-#include <cstddef>
 #include <iomanip>
 
-#include "cirMgr.h"
-#include "glucoseMgr.h"
 #include "gvMsg.h"
 #include "iostream"
-#include "util.h"
 
 bool initExpCmd() {
     return (gvCmdMgr->regCmd("EXPeriment", 3, new ExpCmd));
@@ -25,18 +21,6 @@ extern void testGlucose();
 //----------------------------------------------------------------------
 GVCmdExecStatus ExpCmd::exec(const string& option) {
     //! Place your experimental functions and commands here
-
-    gv_sat::GlucoseMgr* glcMgr = new gv_sat::GlucoseMgr(cirMgr);
-
-    CirGate* realPo = cirMgr->getPo(0)->getIn0Gate();
-    bool invert     = cirMgr->getPo(0)->getIn0().isInv();
-    glcMgr->addBoundedVerifyData(cirMgr->getPo(0), 0);
-    /*glcMgr->assertProperty(realPo, 1, 0);*/
-    if (glcMgr->solve()) {
-        cout << "SAT !!";
-    } else {
-        cout << "UNSAT !!";
-    }
 
     return GV_CMD_EXEC_DONE;
 }
